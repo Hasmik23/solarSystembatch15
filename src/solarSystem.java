03cbe77dce9f9028b0b8fddcfb5dda8bb042c49c
Hasmik23 <ht.ohannesian@gmail.com>
2023-03-28 20:33:35 -0400

    my 1st

---
+++ src/solarSystem.java
@@ -0,0 +1,9 @@
+public class solarSystem {
+    public static void main(String[] args) {
+
+        solarSystem  solar=new solarSystem();
+        solar.sun="1";
+        solar.moom="2";
+        solar.planets="2";
+    }
+}
